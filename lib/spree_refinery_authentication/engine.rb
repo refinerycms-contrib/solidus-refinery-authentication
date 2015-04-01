c69fc155c89000bf46f28013ee228e528d25b230
Brice Sanchez <brice@bisscomm.com>
2015-03-31 21:46:50 -0400

    Change the way we use Spree/Refinery together see https://github.com/refinery/refinerycms/pull/2908

--- lib/spree_refinery_authentication/engine.rb
+++ lib/spree_refinery_authentication/engine.rb
@@ -1,17 +1,11 @@
-require "spree/authentication_helpers"
-
 module SpreeRefineryAuthentication
   class Engine < Rails::Engine
     engine_name "spree_refinery_authentication"
 
     config.to_prepare do
-      Spree.user_class = "Refinery::User"
-
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :per, :per_page
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :num_pages, :total_pages
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :prev_page, :previous_page
-
-      ApplicationController.send :include, Spree::AuthenticationHelpers
     end
   end
 end
