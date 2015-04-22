58bbede7a0c7c45c6d98b5b768ac880cab13fdea
Brice Sanchez <brice@bisscomm.com>
2015-04-22 14:44:23 -0400

    Refactor fix for kaminari/will_paginate

--- lib/spree_refinery_authentication/engine.rb
+++ lib/spree_refinery_authentication/engine.rb
@@ -17,10 +17,26 @@ module SpreeRefineryAuthentication
     engine_name "spree_refinery_authentication"
 
     config.to_prepare do
-      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :per, :per_page
-      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :num_pages, :total_pages
-      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :total_count, :total_entries
-      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :prev_page, :previous_page
+      if defined?(WillPaginate)
+        ::WillPaginate::ActiveRecord::RelationMethods.module_eval do
+          def per_page(num)
+            if (n = num.to_i) <= 0
+              self
+            else
+              limit(n).offset(offset_value / limit_value * n)
+            end
+          end
+
+          def total_pages
+            (total_count.to_f / limit_value).ceil
+          end
+
+          alias_method :per, :per_page
+          alias_method :num_pages, :total_pages
+          alias_method :total_count, :total_entries
+        end
+      end
     end
+
   end
 end
