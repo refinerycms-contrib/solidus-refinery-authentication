ae7f964f725b466901550a807077f2173c89d7f2
Brice Sanchez <brice@bisscomm.com>
2015-04-23 16:21:52 -0400

    Re-add missing alias for prev_page

--- lib/spree_refinery_authentication/engine.rb
+++ lib/spree_refinery_authentication/engine.rb
@@ -34,6 +34,7 @@ module SpreeRefineryAuthentication
           alias_method :per, :per_page
           alias_method :num_pages, :total_pages
           alias_method :total_count, :total_entries
+          alias_method :prev_page, :previous_page
         end
       end
     end
