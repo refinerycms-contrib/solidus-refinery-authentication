801a76b1e5b4bfdf10e6d5a03295accdf91221ac
Brice Sanchez <brice@bisscomm.com>
2015-12-14 10:36:40 -0500

    Add Logout link in refinery site_bar

---
+++ app/overrides/refinery_site_bar.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(virtual_path: "refinery/_site_bar",
+                     name: "refinery_site_bar",
+                     original: "bec20f7641ef24054a38147aeb37f08d31bdd79f",
+                     insert_bottom: "#site_bar_branding",
+                     text: "<%= link_to Spree.t(:logout), spree.logout_path, :id => 'logout' if spree.logout_path.present? %>",
+                     disabled: false)
