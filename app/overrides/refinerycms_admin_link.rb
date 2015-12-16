8e69936b1a2a235c13a94fa6cafa1a4a10b97970
Brice Sanchez <brice@bisscomm.com>
2015-12-16 15:34:54 -0500

    Update tabs for Solidus and Refinery

--- app/overrides/refinerycms_admin_link.rb
+++ app/overrides/refinerycms_admin_link.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
-  virtual_path: 'spree/layouts/admin',
+  virtual_path: 'spree/admin/shared/_menu',
   name: 'refinerycms_admin_link',
   original: '8d05a6b687057aa2a8c08b3feb480d58a14fe4aa',
-  insert_top: '#main-sidebar',
+  insert_top: "[data-hook='admin_tabs']",
   partial: 'spree/admin/shared/refinerycms_sidebar_menu'
 )
--- app/overrides/spree_admin_link.rb
+++ app/overrides/spree_admin_link.rb
@@ -3,5 +3,5 @@ Deface::Override.new(
   name: 'spree_admin_link',
   original: '71468c0788f57fd03c9c6c2ebe3c6c026be5aeb5',
   insert_top: '#menu',
-  text: "<%= link_to 'Spree', spree.admin_path %>"
+  text: "<%= link_to 'Solidus', spree.admin_path %>"
 )
